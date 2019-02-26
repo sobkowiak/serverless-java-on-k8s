ece1861b40c1f87f480657a5c9bd9c3fc2cefd0b
Krzysztof Sobkowiak <krzys.sobkowiak@gmail.com>
2019-02-26 07:17:42 +0100

    Initial import of actions

---
+++ solutions/getstarted/msgaction.js
@@ -0,0 +1,8 @@
+function main(params) {   
+   console.log("JS msgaction args: %j", params);
+   Object.keys(params["body"]).forEach(function(key) {
+       console.log("%s: %j", key, params["body"][key])
+   });
+
+   return { payload: 'Hello ' + params.body.text + ' from JavaScript on OpenWhisk on OpenShift '};
+}
