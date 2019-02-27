fb49553b6126b475d3c4cc3e154e6bcaf1d6833b
Krzysztof Sobkowiak <Krzys.Sobkowiak@gmail.com>
2019-02-27 11:28:33 +0100

    Create trigger-steps.sh

---
+++ solutions/getstarted/trigger-steps.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+wsk  -i action invoke /whisk.system/samples/greeting -r
+wsk  -i action invoke /whisk.system/samples/greeting -r -p name "Tech Talk" -p place Wroclaw
+
+wsk -i trigger create every-5-seconds \
+    --feed  /whisk.system/alarms/alarm \
+    --param cron '*/5 * * * * *' \
+    --param maxTriggers 25 \
+    --param trigger_payload "{\"name\":\"Tech Talk SE\",\"place\":\"Wroclaw\"}"
+
+wsk -i rule create invoke-periodically \
+    every-5-seconds \
+    /whisk.system/samples/greeting
+
+
+wsk -i activation poll
+
+
+wsk -i rule delete invoke-periodically
+wsk -i trigger delete every-5-seconds
