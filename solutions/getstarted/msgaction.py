edaf9701fdbeed4786d9b0f0d29f82c772b1e669
Krzysztof Sobkowiak <krzys.sobkowiak@gmail.com>
2019-10-21 20:09:36 +0200

    msgaction.py corrected

--- solutions/getstarted/msgaction.py
+++ solutions/getstarted/msgaction.py
@@ -1,6 +1,6 @@
 def main(params):
-   print "Py msgaction params: " + str(params)
+   print ("Py msgaction params: " + str(params))
    for key in params["body"]:
-        print key, params["body"][key]        
+        print(key, params["body"][key])
    return { "payload" : 
 	"Hello " + str(params["body"]["text"]) + " from Python on OpenWhisk on OpenShift"}
