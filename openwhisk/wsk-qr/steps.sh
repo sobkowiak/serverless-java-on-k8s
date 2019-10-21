8330087d196df3095423286f58ee8df60b320b3f
Krzysztof Sobkowiak <krzys.sobkowiak@gmail.com>
2019-10-21 22:41:55 +0200

    QR code sample added

---
+++ openwhisk/wsk-qr/steps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git clone https://github.com/rabbah/wsk-qr.git
+cd wsk-qr
+
+
+gradle jar
+wsk -i package create qr
+wsk -i action create qr/generate build/libs/wsk-qr-1.0.jar --main qr.Generate --web true
+wsk -i action create qr/ui src/main/js/qr/ui.js --web true
+
+wsk -i action get qr/ui --url | tail -1 | awk '{print $1".html"}'
+wsk -i action get qr/generate --url | tail -1 | awk '{print $1".http"}'
+WEB_URL=`wsk -i action get qr/generate --url | tail -1 | awk '{print $1".http"}'`
+
+curl -k $WEB_URL?text=Hello --output output.png
+
